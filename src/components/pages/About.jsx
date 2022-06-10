fbbf5f0be3a5cf64672676baa97758af25b576bd
Vazgen Barfyan <vbarfyan@gmail.com>
2022-06-10 13:13:53 +0400

    hi Masha

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
+import {Container} from "@mui/material";
 
 const About = () => {
-  const { t, i18n } = useTranslation();
-  return (
-    <>
-      <div className="about">{t("ourHistory")}</div>
-    </>
-  );
+    const {t, i18n} = useTranslation();
+    return (
+        <Container>
+            <div dangerouslySetInnerHTML={
+                {__html : t('ourHistory', {interpolation : {escapeValue : false}})}
+            }/>
+        </Container>
+    );
 };
 
 export default About;
