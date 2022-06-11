5fdcf80f7f30dddd052059b96e4ed5430c36f884
Mariam <mashagrigorian5@gmail.com>
2022-06-11 21:32:49 +0400

    footer-changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,18 +8,18 @@ const Footer = () => {
 
   return (
     <div className="footer" id="login">
-      <div className="footer-links">
-        <div className="footer-links-contact">
-          <div className="icons">
-            <i className="fa fa-location-dot"></i>
-            <label htmlFor=""> {t("street")}</label>
-            <i className="fa fa-phone"></i>
-            <label htmlFor=""> +374 10 651-631</label>
-            <i className="fa fa-envelope"></i>
-            <label htmlFor="/"> info@cep.am antifraud@epiu.am</label>
-            <label htmlFor=""> {t("copyright")}</label>
-          </div>
+      {/* <div className="footer-links"> */}
+      <div className="footer-links-contact">
+        <div className="icons">
+          <i className="fa fa-location-dot"></i>
+          <label htmlFor=""> {t("street")}</label>
+          <i className="fa fa-phone"></i>
+          <label htmlFor=""> +374 10 651-631</label>
+          <i className="fa fa-envelope"></i>
+          <label htmlFor="/"> info@cep.am antifraud@epiu.am</label>
+          <label htmlFor=""> {t("copyright")}</label>
         </div>
+        {/* </div> */}
       </div>
 
       {/* <div className="footer-copyright">
