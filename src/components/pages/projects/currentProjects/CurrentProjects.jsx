a5c324251b0bfb53ede1d0e51066beff02f25e50
Vazgen Barfyan <vbarfyan@gmail.com>
2022-12-12 15:25:19 +0800

    minor fixes

--- src/components/pages/projects/currentProjects/CurrentProjects.jsx
+++ src/components/pages/projects/currentProjects/CurrentProjects.jsx
@@ -131,46 +131,32 @@
 // export default CurrentProjects;
 
 import * as React from "react";
-import { itemData } from "./ItemData";
-import { Link } from "react-router-dom";
+import {itemData} from "./ItemData";
+import {Link} from "react-router-dom";
 import ImageListItem from "@mui/material/ImageListItem";
-import { useTranslation } from "react-i18next";
-import Grid from "@mui/material/Grid";
+import {useTranslation} from "react-i18next";
 import Paper from "@mui/material/Paper";
 import ImageList from "@mui/material/ImageList";
-import Box from "@mui/material/Box";
 
 const CurrentProjects = () => {
-  const { t, i18n } = useTranslation();
+    const {t, i18n} = useTranslation();
 
-  return (
-    <div style={{ marginTop: "10%" }}>
-      <Grid container spacing={2}>
-        <Box
-          sx={{
-            display: "flex",
-            "& > :not(style)": {
-              m: 1,
-              width: 128,
-              height: 128,
-            },
-          }}
-        >
-          <Paper variant="outlined" />
-          <Paper variant="outlined" square />
-        </Box>
-        <ImageList
-          gap={20}
-          sx={{
-            mb: 8,
-          }}
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-          }}
-        >
-          {/* {itemData.map((item) => (
+    return (
+        <div style={{marginTop: "8%"}}>
+            <ImageList
+                gap={100}
+                sx={{
+
+                    pt:15,
+                    pb:15
+                }}
+                style={{
+                    display: 'flex',
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                }}
+            >
+                {/* {itemData.map((item) => (
                      <Link to={"/projects/current-projects/" + item.key}>
                         <ImageListItem
                           key={item.img}
@@ -178,62 +164,64 @@ const CurrentProjects = () => {
                             position: "relative",
                            }}
                       > */}
-          {itemData.map((item) => (
-            <Link to={"/projects/current-projects/" + item.key}>
-              <ImageListItem
-                key={item.img}
-                style={{
-                  position: "relative",
-                }}
-              >
-                <div
-                  style={{
-                    position: "absolute",
-                    left: 7,
-                    top: 5,
-                    background: "white",
-                    opacity: "0.70",
-                    color: "black",
-                    fontWeight: "bold",
-                    borderRadius: "3px",
-                    padding: "3px 5px",
-                  }}
-                >
-                  <div>{t(`allCurrentProject.${item.key}.projectTag`)}</div>
-                </div>
-                <img
-                  style={{
-                    width: "550px",
-                    height: "500px",
-                  }}
-                  src={`${item.img}?w=248&fit=crop&auto=format`}
-                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                  alt={item.title}
-                />
-                <div
-                  style={{
-                    position: "absolute",
-                    left: 7,
-                    bottom: 30,
-                    background: "white",
-                    opacity: "0.70",
-                    color: "black",
-                    borderRadius: "3px",
-                    fontWeight: "bold",
-                    padding: "3px 5px",
-                  }}
-                >
-                  <div>{t(`allCurrentProject.${item.key}.currentProject`)}</div>
-                </div>
-              </ImageListItem>
-            </Link>
-          ))}
-          {/* </ImageListItem>
+                {(itemData || []).map((item) => (
+                    <Paper square key={item.key} elevation={15}>
+                        <Link to={"/projects/current-projects/" + item.key}>
+                            <ImageListItem
+                                key={item.img}
+                                style={{
+                                    position: "relative",
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        position: "absolute",
+                                        left: 7,
+                                        top: 5,
+                                        background: "white",
+                                        opacity: "0.70",
+                                        color: "black",
+                                        fontWeight: "bold",
+                                        borderRadius: "3px",
+                                        padding: "3px 5px",
+                                    }}
+                                >
+                                    <div>{t(`allCurrentProject.${item.key}.projectTag`)}</div>
+                                </div>
+                                <img
+                                    style={{
+                                        width: "550px",
+                                        height: "500px",
+                                    }}
+                                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={item.title}
+                                />
+                                <div
+                                    style={{
+                                        position: "absolute",
+                                        left: 7,
+                                        bottom: 30,
+                                        right :7,
+                                        background: "white",
+                                        opacity: "0.70",
+                                        color: "black",
+                                        borderRadius: "3px",
+                                        fontWeight: "bold",
+                                        padding: "3px 5px",
+                                    }}
+                                >
+                                    <div>{t(`allCurrentProject.${item.key}.currentProject`)}</div>
+                                </div>
+                            </ImageListItem>
+                        </Link>
+                    </Paper>
+                ))}
+                {/* </ImageListItem>
                   </Link> */}
-        </ImageList>
-      </Grid>
-    </div>
-  );
+            </ImageList>
+        </div>
+    );
 };
 
 export default CurrentProjects;
